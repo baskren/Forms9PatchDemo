56c93a4f56103b3408cdb074b2ac79ee8b07a404
Ben Askren <ben@buildcalc.com>
2016-01-21 10:24:46 -0500

    Added XAML demos for the ContentView and Frame elements

---
+++ Forms9PatchDemo/ContentViewDemoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Forms9PatchDemo
+{
+	public partial class ContentViewDemoPage : ContentPage
+	{
+		public ContentViewDemoPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
--- Forms9PatchDemo/Forms9PatchDemo.cs
+++ Forms9PatchDemo/Forms9PatchDemo.cs
@@ -6,16 +6,19 @@ namespace Forms9PatchDemo
 	{
 		public App ()
 		{
-			const bool XAML = false;
+			const bool XAML = true;
 
 			if (XAML) {
-				MainPage = new MyPage ();
+				//MainPage = new MyPage ();
+				//MainPage = new ContentViewDemoPage ();
+				MainPage = new FrameDemoPage ();
 			} else {
 				const double fontSize = 9;
 				MainPage = new ContentPage {
-					Content = new ScrollView {
+					//Content = new ScrollView {
 						Content = new StackLayout {
 							Children = {
+								/*
 								#region Original Image
 								new Forms9Patch.Image {
 									Source = ImageSource.FromResource("Forms9PatchDemo.Resources.bubble.9.png"),
@@ -129,7 +132,6 @@ namespace Forms9PatchDemo
 									HorizontalOptions = LayoutOptions.Center,
 								},
 								#endregion
-
 								#region Image
 								new Label {
 									Text = "Forms9Patch.Image",
@@ -323,53 +325,45 @@ namespace Forms9PatchDemo
 									Padding = new Thickness(10),
 									BackgroundColor = Color.Gray,
 								},
-								/*
-						new Forms9Patch.Frame {
-							BackgroundImage = new Forms9Patch.Image {
-								Source = Forms9Patch.ImageSource.FromMultiResource ("Forms9PatchDemo.Resources.redGridBox"),
-								Fill = Forms9Patch.Fill.AspectFit,
-							},
-							Content = new Xamarin.Forms.Label {
-								Text = "Tlkjasdflkjasdlkfjsdflsadjfsladfsdf",
-								TextColor = Color.Black,
-								FontSize = 12,
-								BackgroundColor = Color.Green,
-							},
-							Padding = new Thickness(10),
-						},
-						new Forms9Patch.Frame {
-							BackgroundImage = new Forms9Patch.Image {
-								Source = Forms9Patch.ImageSource.FromMultiResource ("Forms9PatchDemo.Resources.redGridBox"),
-								Fill = Forms9Patch.Fill.Fill,
-							},
-							Content = new Xamarin.Forms.Label {
-								Text = "Tlkjasdflkjasdlkfjsdflsadjfsladfsdf",
-								TextColor = Color.Black,
-								FontSize = 12,
-								BackgroundColor = Color.Green,
-							},
-							Padding = new Thickness(10),
-						},
-						new Forms9Patch.Frame {
-							BackgroundImage = new Forms9Patch.Image {
-								Source = Forms9Patch.ImageSource.FromMultiResource ("Forms9PatchDemo.Resources.redGridBox"),
-								Fill = Forms9Patch.Fill.Tile,
-							},
-							Content = new Xamarin.Forms.Label {
-								Text = "Tlkjasdflkjasdlkfjsdflsadjfsladfsdf",
-								TextColor = Color.Black,
-								FontSize = 12,
-								BackgroundColor = Color.Green,
-							},
-							Padding = new Thickness(10),
-						},
-						*/
 								#endregion
 
-
+								#region CapsInset ContentView
+								new Image { 
+									Source = Forms9Patch.ImageSource.FromMultiResource("Forms9PatchDemo.Resources.redribbon"), 
+								},
+								new Label { Text = "Forms9Patch.ImageSource.FromMultiSource >> Xamarin.Forms.Image", 
+									FontSize = fontSize,
+									HorizontalOptions = LayoutOptions.Center,
+								},
+								*/
+								new Forms9Patch.ContentView {
+									BackgroundImage = new Forms9Patch.Image {
+										Source = Forms9Patch.ImageSource.FromMultiResource ("Forms9PatchDemo.Resources.redribbon"),
+										Fill = Forms9Patch.Fill.Tile,
+										CapInsets = new Thickness(30,-1,160,-1),
+									},
+									Content = new Xamarin.Forms.Label{
+										Text = "ContentView scalable (CapInsets)",
+										TextColor = Color.White,
+										FontAttributes = FontAttributes.Bold,
+										//BackgroundColor = Color.Gray,
+										FontSize = 14,
+										HorizontalOptions = LayoutOptions.Center,
+										VerticalOptions = LayoutOptions.Center,
+									},
+									Padding = new Thickness(30,30,110,20),
+									HeightRequest = 80,
+								},
+							/*
+								new Label { Text = "Forms9atch.ImageSource.FromMultiSource >> Forms9Patch.ContentView", 
+									FontSize = fontSize,
+									HorizontalOptions = LayoutOptions.Center,
+								},
+								#endregion
+*/
 							}
 						}
-					}
+					//}
 				};
 				MainPage.Padding = new Thickness (5, Device.OnPlatform(20,5,5), 5, 5);			
 			}
