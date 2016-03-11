944ae5372bc1bc692df3634b377dd42ea427d978
Ben Askren <ben@buildcalc.com>
2016-03-11 16:25:12 -0500

    0.9.10 - Includes demos of new ModalPopup and BubblePopup elements.

--- Forms9PatchDemo/HomePage.cs
+++ Forms9PatchDemo/HomePage.cs
@@ -9,84 +9,82 @@ namespace Forms9PatchDemo
 		{
 			// Define command for the items in the TableView.
 			Command<Type> navigateCommand = 
-				new Command<Type>(async (Type pageType) =>
-					{
+				new Command<Type>(async (Type pageType) => {
 						Page page = (Page)Activator.CreateInstance(pageType);
 						await this.Navigation.PushAsync(page);
 					});
 
 			this.Title = "Forms Gallery";
-			this.Content = new TableView
-			{
+			this.Content = new TableView {
 				Intent = TableIntent.Menu,
-				Root = new TableRoot
-				{
-					new TableSection("XAML")
-					{
-						new TextCell
-						{
+				Root = new TableRoot {
+					new TableSection("XAML") {
+						new TextCell {
 							Text = "ContentView",
 							Command = navigateCommand,
 							CommandParameter = typeof(ContentViewDemoPage)
 						},
 
-						new TextCell
-						{
+						new TextCell {
 							Text = "Frame",
 							Command = navigateCommand,
 							CommandParameter = typeof(FrameDemoPage)
 						},
 
-						new TextCell
-						{
+						new TextCell {
 							Text = "ImageButton",
 							Command = navigateCommand,
 							CommandParameter = typeof(ImageButtonPage)
 						},
 
-						new TextCell
-						{
+						new TextCell {
 							Text = "MaterialSegmentControl ",
 							Command = navigateCommand,
 							CommandParameter = typeof(MaterialSegmentControlPage)
 						},
 
-						new TextCell
-						{
+						new TextCell {
 							Text = "Image",
 							Command = navigateCommand,
 							CommandParameter = typeof(MyPage)
 						}
 					}, 
 
-					new TableSection("Code")
-					{
-						new TextCell
-						{
+					new TableSection("Code") {
+						new TextCell {
 							Text = "ImageButton",
 							Command = navigateCommand,
 							CommandParameter = typeof(ImageButtonCodePage)
 						},
 
-						new TextCell
-						{
+						new TextCell {
 							Text = "Image",
 							Command = navigateCommand,
 							CommandParameter = typeof(ImageCodePage)
 						},
 
-						new TextCell
-						{
+						new TextCell {
 							Text = "Layouts",
 							Command = navigateCommand,
 							CommandParameter = typeof(LayoutsPage)
 						},
 
-						new TextCell
-						{
+						new TextCell {
 							Text = "Material Buttons",
 							Command = navigateCommand,
 							CommandParameter = typeof(MaterialButtonsPage)
+						},
+
+						new TextCell {
+							Text = "Modal Popup",
+							Command = navigateCommand,
+							CommandParameter = typeof(ModalPopupTestPage),
+						},
+
+						new TextCell {
+							Text = "Bubble Popup",
+							Command = navigateCommand,
+							CommandParameter = typeof(BubblePopupTestPage),
 						},
 
 					}
