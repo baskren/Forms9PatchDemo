d55ec3f597f476ccbff563a9ced0bf9496c8e5e3
Ben Askren <ben@buildcalc.com>
2016-09-01 10:51:31 -0400

    0.9.13.1: Added CancelOnBackgroundTouch to ModalPopup and BubblePopup

--- Forms9PatchDemo/Pages/ModalPopupTestPage.cs
+++ Forms9PatchDemo/Pages/ModalPopupTestPage.cs
@@ -6,88 +6,117 @@ namespace Forms9PatchDemo
 {
 	public class ModalPopupTestPage : MasterDetailPage
 	{
-		public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create("CornerRadius",typeof(double), typeof(ModalPopupTestPage), 4.0);
-		public double CornerRadius {
-			get { return (double) GetValue(CornerRadiusProperty); }
-			set { SetValue (CornerRadiusProperty, value); }
+		public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create("CornerRadius", typeof(double), typeof(ModalPopupTestPage), 4.0);
+		public double CornerRadius
+		{
+			get { return (double)GetValue(CornerRadiusProperty); }
+			set { SetValue(CornerRadiusProperty, value); }
 		}
 
-		public static readonly BindableProperty PUDPaddingProperty = BindableProperty.Create("PUDPadding",typeof(double), typeof(ModalPopupTestPage), 10.0);
-		public double PUDPadding {
-			get { return (double) GetValue(PUDPaddingProperty); }
-			set { 
-				SetValue (PUDPaddingProperty, value); 
-				PUPadding = new Thickness (value);
+		public static readonly BindableProperty PUDPaddingProperty = BindableProperty.Create("PUDPadding", typeof(double), typeof(ModalPopupTestPage), 10.0);
+		public double PUDPadding
+		{
+			get { return (double)GetValue(PUDPaddingProperty); }
+			set
+			{
+				SetValue(PUDPaddingProperty, value);
+				PUPadding = new Thickness(value);
 			}
 		}
 
-		public static readonly BindableProperty PUPaddingProperty = BindableProperty.Create("PUPadding",typeof(Thickness), typeof(ModalPopupTestPage), new Thickness((double)ModalPopupTestPage.PUDPaddingProperty.DefaultValue));
-		public Thickness PUPadding {
-			get { return (Thickness) GetValue(PUPaddingProperty); }
-			set { SetValue (PUPaddingProperty, value); }
+		public static readonly BindableProperty PUPaddingProperty = BindableProperty.Create("PUPadding", typeof(Thickness), typeof(ModalPopupTestPage), new Thickness((double)ModalPopupTestPage.PUDPaddingProperty.DefaultValue));
+		public Thickness PUPadding
+		{
+			get { return (Thickness)GetValue(PUPaddingProperty); }
+			set { SetValue(PUPaddingProperty, value); }
 		}
 
 		public static readonly BindableProperty HasShadowProperty = BindableProperty.Create("HasShadow", typeof(bool), typeof(ModalPopupTestPage), false);
-		public bool HasShadow {
-			get { return (bool) GetValue(HasShadowProperty); }
-			set { SetValue (HasShadowProperty, value); }
+		public bool HasShadow
+		{
+			get { return (bool)GetValue(HasShadowProperty); }
+			set { SetValue(HasShadowProperty, value); }
 		}
 
 		public static readonly BindableProperty ShadowInvertedProperty = BindableProperty.Create("ShadowInverted", typeof(bool), typeof(ModalPopupTestPage), false);
-		public bool ShadowInverted {
-			get { return (bool) GetValue(ShadowInvertedProperty); }
-			set { SetValue (ShadowInvertedProperty, value); }
+		public bool ShadowInverted
+		{
+			get { return (bool)GetValue(ShadowInvertedProperty); }
+			set { SetValue(ShadowInvertedProperty, value); }
 		}
 
 		public static readonly BindableProperty OutlineWidthProperty = BindableProperty.Create("OutlineWidth", typeof(float), typeof(ModalPopupTestPage), 0f);
-		public float OutlineWidth {
-			get { return (float) GetValue(OutlineWidthProperty); }
-			set { SetValue (OutlineWidthProperty, value); }
+		public float OutlineWidth
+		{
+			get { return (float)GetValue(OutlineWidthProperty); }
+			set { SetValue(OutlineWidthProperty, value); }
 		}
 
-		public ModalPopupTestPage ()
+		public static readonly BindableProperty CancelOnBackgroundTouchProperty = BindableProperty.Create("CancelOnBackgroundTouch", typeof(bool), typeof(ModalPopupTestPage), true);
+		public bool CancelOnBackgroundTouch
+		{
+			get { return (bool)GetValue(CancelOnBackgroundTouchProperty); }
+			set { SetValue(CancelOnBackgroundTouchProperty, value); }
+		}
+
+
+		public ModalPopupTestPage()
 		{
 			BackgroundColor = Color.White;
-			Padding = new Thickness(20,Device.OnPlatform(20,0,0),20,20);
+			Padding = new Thickness(20, Device.OnPlatform(20, 0, 0), 20, 20);
 
-			var shadowToggle = new Switch ();
-			shadowToggle.SetBinding (Switch.IsToggledProperty, "HasShadow");
+			var shadowToggle = new Switch();
+			shadowToggle.SetBinding(Switch.IsToggledProperty, "HasShadow");
 			shadowToggle.BindingContext = this;
 
-			var shadowInvertedToggle = new Switch ();
-			shadowInvertedToggle.SetBinding (Switch.IsToggledProperty, "ShadowInverted");
+			var shadowInvertedToggle = new Switch();
+			shadowInvertedToggle.SetBinding(Switch.IsToggledProperty, "ShadowInverted");
 			shadowInvertedToggle.BindingContext = this;
 
-			var cornerRadiusSlider = new Slider {
+			var cornerRadiusSlider = new Slider
+			{
 				Maximum = 40,
 				Minimum = 0,
 				HeightRequest = 20,
 			};
-			cornerRadiusSlider.SetBinding (Slider.ValueProperty, "CornerRadius");
+			cornerRadiusSlider.SetBinding(Slider.ValueProperty, "CornerRadius");
 			cornerRadiusSlider.BindingContext = this;
 
-			var paddingSlider = new Slider {
+			var paddingSlider = new Slider
+			{
 				Maximum = 100,
 				Minimum = 0,
 				HeightRequest = 20,
 			};
-			paddingSlider.SetBinding (Slider.ValueProperty, "PUDPadding");
+			paddingSlider.SetBinding(Slider.ValueProperty, "PUDPadding");
 			paddingSlider.BindingContext = this;
 
 			//var listener = new FormsOrientation.Listener ();
 
-			var hidePopupButton = new Forms9Patch.MaterialButton {
+			var hidePopupButton = new Forms9Patch.MaterialButton
+			{
 				Text = "DONE",
 				BackgroundColor = Color.Blue,
 				DarkTheme = true,
 			};
-			var showPopupButton = new Forms9Patch.MaterialButton {
+			var showPopupButton = new Forms9Patch.MaterialButton
+			{
 				Text = "Show Modal popup",
 				BackgroundColor = Color.Blue,
 				DarkTheme = true,
 				WidthRequest = 150,
 				HorizontalOptions = LayoutOptions.Center,
 			};
+
+			var cancelOnBackgroundTouchButton = new Forms9Patch.MaterialButton
+			{
+				Text = "Cancel on Background touch",
+				StickyBehavior = true,
+				IsSelected = true
+			};
+			cancelOnBackgroundTouchButton.SetBinding(Forms9Patch.MaterialButton.IsSelectedProperty, "CancelOnBackgroundTouch");
+			cancelOnBackgroundTouchButton.BindingContext = this;
+
 
 			var blackSegment = new Forms9Patch.Segment { HtmlText = "<font color=\"#000000\">Black</font>" };
 			var redSegment = new Forms9Patch.Segment { HtmlText = "<font color=\"#FF0000\">Red</font>" };
@@ -105,12 +134,13 @@ namespace Forms9PatchDemo
 				BackgroundColor = Color.White
 			};
 
-
-			var modal = new Forms9Patch.ModalPopup {
-				Content = new StackLayout {
+			var modal = new Forms9Patch.ModalPopup
+			{
+				Content = new StackLayout
+				{
 					Children = {
-						new Label { 
-							Text = "Hello Modal popup!", 
+						new Label {
+							Text = "Hello Modal popup!",
 							TextColor = Color.Black,},
 						new Label { Text = "Padding:", FontSize=10, },
 						paddingSlider,
@@ -128,21 +158,26 @@ namespace Forms9PatchDemo
 				OutlineColor = Color.Black,
 				BackgroundColor = Color.Aqua,
 				HasShadow = true,
+				HeightRequest = 200,
+				WidthRequest = 200,
 			};
-			modal.SetBinding (Forms9Patch.ModalPopup.OutlineRadiusProperty, "CornerRadius");
-			modal.SetBinding (Forms9Patch.ModalPopup.OutlineWidthProperty, "OutlineWidth");
-			modal.SetBinding (Forms9Patch.ModalPopup.PaddingProperty, "PUPadding");
-			modal.SetBinding (Forms9Patch.ModalPopup.HasShadowProperty, "HasShadow");
-			modal.SetBinding (Forms9Patch.ModalPopup.ShadowInvertedProperty, "ShadowInverted");
+			modal.SetBinding(Forms9Patch.ModalPopup.OutlineRadiusProperty, "CornerRadius");
+			modal.SetBinding(Forms9Patch.ModalPopup.OutlineWidthProperty, "OutlineWidth");
+			modal.SetBinding(Forms9Patch.ModalPopup.PaddingProperty, "PUPadding");
+			modal.SetBinding(Forms9Patch.ModalPopup.HasShadowProperty, "HasShadow");
+			modal.SetBinding(Forms9Patch.ModalPopup.ShadowInvertedProperty, "ShadowInverted");
+			modal.SetBinding(Forms9Patch.ModalPopup.CancelOnPageOverlayTouchProperty, "CancelOnBackgroundTouch");
 			modal.BindingContext = this;
 
 
-			showPopupButton.Tapped += (sender, e) => {
+			showPopupButton.Tapped += (sender, e) =>
+			{
 				modal.IsVisible = true;
 			};
-			hidePopupButton.Tapped += (sender, e) => {
+			hidePopupButton.Tapped += (sender, e) =>
+			{
 				modal.IsVisible = false;
-				System.Diagnostics.Debug.WriteLine("button "+showPopupButton.Text);
+				System.Diagnostics.Debug.WriteLine("button " + showPopupButton.Text);
 			};
 
 			blackSegment.Selected += (sender, e) => { modal.PageOverlayColor = Color.FromRgba(0, 0, 0, 128); };
@@ -150,14 +185,14 @@ namespace Forms9PatchDemo
 			greenSegment.Selected += (sender, e) => { modal.PageOverlayColor = Color.FromRgba(0, 255, 0, 128); };
 			blueSegment.Selected += (sender, e) => { modal.PageOverlayColor = Color.FromRgba(0, 0, 255, 128); };
 
-
-
-			Detail = new ContentPage {
+			Detail = new ContentPage
+			{
 				BackgroundColor = Color.White,
 				Title = "PopupTestPage - Detail",
-				Content = new StackLayout { 
+				Content = new StackLayout
+				{
 					Children = {
-						new Label { 
+						new Label {
 							Text = "Hello ContentPage",
 							TextColor = Color.Black,
 						},
@@ -180,15 +215,18 @@ namespace Forms9PatchDemo
 								}
 							}
 						},
+						cancelOnBackgroundTouchButton,
 						showPopupButton,
 					},
 					Padding = 20,
 				},
-			};	
+			};
 
-			Master = new ContentPage {
+			Master = new ContentPage
+			{
 				Title = "PopupTestPage - Master",
-				Content = new Label {
+				Content = new Label
+				{
 					Text = "Master Page",
 				},
 				BackgroundColor = Color.Gray,
