2681e396efc31f5cd94a11f005e6647682a5c2a4
Ben Askren <ben@buildcalc.com>
2017-08-31 08:27:11 -0400

    0.9.10.3

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,19 +10,20 @@ using Android.OS;
 
 namespace Forms9PatchDemo.Droid
 {
-	[Activity(Label = "Forms9Patch Demo", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
+    [Activity(Label = "asap - The Ascendas-Singbridge App", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
 
-			Forms9Patch.Droid.Settings.Initialize("NZPK-RMP4-PJVV-Z7LP-78JF-GNXB-CDJZ-SRYA-BLR2-WBZC-G64K-QJZW-65DB");
+            //Forms9Patch.Droid.Settings.Initialize("NZPK-RMP4-PJVV-Z7LP-78JF-GNXB-CDJZ-SRYA-BLR2-WBZC-G64K-QJZW-65DB");  // for "Forms9Patch Demo"
+            Forms9Patch.Droid.Settings.Initialize("EGC8-5NV9-EAWV-3859-4SKY-PQW3-S6SJ-RYN3-JF3R-2P9D-866F-AVT6-52G7");  // for "asap - The Ascendas-Singbridge App"
 
-			LoadApplication(new App());
-		}
-	}
+            LoadApplication(new App());
+        }
+    }
 }
 
