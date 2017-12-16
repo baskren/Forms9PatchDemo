771af00b629003c786f92fb1980779a448e7c22a
DESKTOP-SO17ACK\ben <ben@buildcalc.com>
2017-12-15 21:43:18 -0500

    UWP and Android working on Windwos

--- Forms9PatchDemo.Droid.OLD/MainActivity.cs
+++ Forms9PatchDemo.Droid.OLD/MainActivity.cs
@@ -1,4 +1,4 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
 using Android.Views;
 using Android.OS;
@@ -27,27 +27,4 @@ namespace Forms9PatchDemo.Droid
         }
     }
 }
-
-
-
-/*
-using Android.App;
-using Android.Widget;
-using Android.OS;
-
-namespace Forms9PatchDemo.Droid
-{
-    [Activity(Label = "Forms9PatchDemo.Droid", MainLauncher = true)]
-    public class MainActivity : Activity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-        }
-    }
-}
-*/
 
