982ad11b72ead06afb4ebb0ba2d292861ae938fe
Ben Askren <ben@buildcalc.com>
2018-05-01 11:18:14 -0400

    Small update to Gestures page to improve debugging.

--- Source/Pages.Code/GestureTestPage.cs
+++ Source/Pages.Code/GestureTestPage.cs
@@ -26,6 +26,7 @@ namespace Forms9PatchDemo
 
 
             var buttonListener = FormsGestures.Listener.For(button);
+
             buttonListener.Down += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBUTTON DOWN [" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]"); // does not work with UIControl derived elements
             buttonListener.RightClicked += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBUTTON RIGHT CLICK[" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
 
@@ -57,6 +58,7 @@ namespace Forms9PatchDemo
 
             buttonListener.Pinched += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBUTTON PINCHED TotalScale=[" + e.TotalScale + "][" + e.Center + "][" + e.ViewPosition + "]");
             buttonListener.Rotated += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBUTTON ROTATED TotalAngle=[" + e.TotalAngle + "][" + e.Center + "][" + e.ViewPosition + "]");
+
             #endregion
 
 
@@ -67,8 +69,8 @@ namespace Forms9PatchDemo
             };
             var boxListener = FormsGestures.Listener.For(box);
 
-            //boxListener.Down += OnDown;
-            boxListener.Down += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX DOWN [" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
+            boxListener.Down += OnDown;
+            //boxListener.Down += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX DOWN [" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
             boxListener.LongPressing += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX LONG PRESSING [" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
             boxListener.RightClicked += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX RIGHT CLICK[" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
 
@@ -90,7 +92,8 @@ namespace Forms9PatchDemo
                 box.Rotation += e.DeltaAngle;
             };
 
-            boxListener.Up += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX UP [" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
+            //boxListener.Up += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX UP [" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
+            boxListener.Tapping += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX TAPPING #[" + e.NumberOfTaps + "] [" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
             boxListener.Tapped += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX TAPPED #[" + e.NumberOfTaps + "] [" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
             boxListener.DoubleTapped += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX DOUBLE TAPPED [" + e.Center + "][" + e.Touches[0] + "][" + e.ViewPosition + "]");
 
@@ -100,6 +103,7 @@ namespace Forms9PatchDemo
 
             boxListener.Pinched += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX PINCHED TotalScale=[" + e.TotalScale + "][" + e.Center + "][" + e.ViewPosition + "]");
             boxListener.Rotated += (sender, e) => System.Diagnostics.Debug.WriteLine("\tBOX ROTATED TotalAngle[" + e.TotalAngle + "][" + e.Center + "][" + e.ViewPosition + "]");
+
             #endregion
 
 
@@ -109,9 +113,11 @@ namespace Forms9PatchDemo
                 BackgroundColor = Color.Blue,
             };
             var tapBoxListener = FormsGestures.Listener.For(tapBox);
+
             tapBoxListener.Tapping += (sender, e) => System.Diagnostics.Debug.WriteLine("\tTAPBOX TAPPING #[" + e.NumberOfTaps + "]");
             tapBoxListener.Tapped += (sender, e) => System.Diagnostics.Debug.WriteLine("\tTAPBOX TAPPED #[" + e.NumberOfTaps + "]");
             tapBoxListener.DoubleTapped += (sender, e) => System.Diagnostics.Debug.WriteLine("\tTAPBOX DOUBLE TAPPED #[" + e.NumberOfTaps + "]");
+
             #endregion
 
             #region RelativeLayout
@@ -125,16 +131,16 @@ namespace Forms9PatchDemo
             relativeLayout.Children.Clear();
 
             relativeLayout.Children.Add(button,
-                xConstraint: Constraint.RelativeToParent((parent) => { return parent.X + parent.Width / 2; }),
-                yConstraint: Constraint.RelativeToParent((parent) => { return parent.Y + parent.Height / 2; }),
-                widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width / 8; }),
-                heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Width / 8; })
+                xConstraint: Constraint.RelativeToParent((parent) => { return parent.X + parent.Width * 3 / 4; }),
+                yConstraint: Constraint.RelativeToParent((parent) => { return parent.Y + parent.Height * 3 / 4; }),
+                widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width / 3; }),
+                heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Width / 3; })
             );
             relativeLayout.Children.Add(box,
                 xConstraint: Constraint.RelativeToParent((parent) => { return parent.X + parent.Width / 4; }),
                 yConstraint: Constraint.RelativeToParent((parent) => { return parent.Y + parent.Height / 4; }),
-                widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width / 3; }),
-                heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Width / 3; })
+                widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width / 2; }),
+                heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Width / 2; })
             );
             relativeLayout.Children.Add(tapBox,
                 xConstraint: Constraint.RelativeToParent((parent) => { return parent.X + parent.Width / 8; }),
