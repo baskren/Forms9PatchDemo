7ad81c0f8d503553a3457885d322f5379488b69c
Ben Askren <ben@buildcalc.com>
2018-10-19 20:36:57 -0400

    Added scale to the XamlSingleButton page

--- Source/Pages.Xaml/XamlSingleStateButtonPage.xaml.cs
+++ Source/Pages.Xaml/XamlSingleStateButtonPage.xaml.cs
@@ -8,19 +8,25 @@ namespace Forms9PatchDemo
 {
     //[XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class XamlSingleStateButtonPage : ContentPage
-	{
-		public XamlSingleStateButtonPage ()
-		{
-			InitializeComponent ();
-		}
+    {
+        public XamlSingleStateButtonPage()
+        {
+            InitializeComponent();
+        }
 
-		protected override void OnDisappearing()
-		{
-			BindingContext = null;
-			Content = null;
-			base.OnDisappearing();
-			GC.Collect();
-		}
-	}
+        protected override void OnDisappearing()
+        {
+            BindingContext = null;
+            Content = null;
+            base.OnDisappearing();
+            GC.Collect();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            label.Text = "Scale=" + Forms9Patch.Display.Scale;
+        }
+    }
 }
 
