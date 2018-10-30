657d4131864d3ec0c3f8b711e5da3dc435037da0
Ben Askren <ben@buildcalc.com>
2018-10-30 09:05:56 -0400

    Added XAML Compile to GridPage

--- Source/Pages.Xaml/GridPage.xaml.cs
+++ Source/Pages.Xaml/GridPage.xaml.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Forms9PatchDemo
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GridPage : ContentPage
     {
         public GridPage()
