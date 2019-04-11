066424e21725dd61a7f0406b479f711d1fef6aed
Ben Askren <ben@buildcalc.com>
2019-04-11 14:38:15 -0400

    Updated HtmlLink to demonstrate using [ ' ] character as quote for tag attributes;

--- Source/Pages.Code/HtmlLink.cs
+++ Source/Pages.Code/HtmlLink.cs
@@ -11,12 +11,21 @@ namespace Forms9PatchDemo
                 HtmlText = "Contact us on <a id=\"phone\" href=\"tel:+353015546889\">015546889</a> or <a id=\"email\" href=\"mailto:email@hotmail.com\">email@hotmail.com</a>",
                 HorizontalTextAlignment = TextAlignment.Center
             };
+            var label2 = new Forms9Patch.Label
+            {
+                HtmlText = @"08 Apr 2019 15:45:50:870 <b>HTML</b> <big>really big text</big><a href='http://somesite.gov.ua' target='_blank' title='Official site'>Official site</a>",
+                HorizontalTextAlignment = TextAlignment.Center
+            };
             label.ActionTagTapped += (object sender, Forms9Patch.ActionTagEventArgs e) =>
             {
                 var id = e.Id;
                 var href = e.Href;
                 var uri = new Uri(e.Href);
                 Device.OpenUri(uri);
+            };
+            label2.ActionTagTapped += (sender, e) =>
+            {
+                Forms9Patch.Toast.Create("Action", e.Href);
             };
             var stack = new StackLayout
             {
@@ -25,6 +34,8 @@ namespace Forms9PatchDemo
                     new Forms9Patch.Label("Forms9Patch.Label.HtmlText &lt;a&gt; example.") { HorizontalTextAlignment = TextAlignment.Center },
                     new BoxView { BackgroundColor = Color.Black, HeightRequest = 1 },
                     label,
+                    new BoxView { BackgroundColor = Color.Black, HeightRequest = 1 },
+                    label2,
                     new BoxView { BackgroundColor = Color.Black, HeightRequest = 1 },
                 }
             };
